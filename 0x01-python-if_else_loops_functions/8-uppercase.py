01eed039b854b5997f36eca6963de545a3d7d215
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-06-29 00:34:47 +0100

    Fix from f string to string format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for char in str:
         if 97 <= ord(char) <= 122:
             char = chr((ord(char) - ord("a")) + ord("A"))
-        print(char, end="")
+        print("{}".format(char), end="")
     print("")
