bcf1e8f139b71bfcefe0db553b038decd819f02c
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-06-28 12:27:23 +0100

    Fix to follow pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = number - int(number / 10) * 10
 if last_digit != 0 and last_digit < 6:
-    decision  = "and is less than 6 and not 0"
+    decision = "and is less than 6 and not 0"
 elif last_digit > 5:
     decision = "and is greater than 5"
 elif last_digit == 0:
