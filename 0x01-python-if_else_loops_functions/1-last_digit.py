da8ad7c15e7c088c87bc788d327c203018f013ae
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-06-28 12:16:13 +0100

    Python script that checks if the last digit overcomes a set of checks

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = number - int(number / 10) * 10
+if last_digit != 0 and last_digit < 6:
+    decision  = "and is less than 6 and not 0"
+elif last_digit > 5:
+    decision = "and is greater than 5"
+elif last_digit == 0:
+    decision = "and is 0"
+print(f"Last digit of {number:d} is {last_digit:d} {decision}")
