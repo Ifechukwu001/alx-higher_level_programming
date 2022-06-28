838942357e2cdcebaf1501baf4a0a995c9779aff
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-06-29 00:55:47 +0100

    Actually i got this new concept from someone, its pretty neat. PS. it was modified

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        print("{:d}{:d}".format(i, j), end="")
-        if i == 9 and j == 9:
-            end_char = "\n"
-        else:
-            end_char = ", "
-        print(end_char, end="")
+for i in range(100):
+    if i != 99:
+        print("{:d}{:d}".format(int(i / 10), i % 10), end=", ")
+    else:
+        print("{:d}{:d}".format(int(i / 10), i % 10))
