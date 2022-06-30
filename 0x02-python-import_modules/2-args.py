ec33e45b7509de4cf5785d8058fca3ac1f9280d4
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-06-30 11:05:16 +0100

    Python script that prints out arguments passed to the script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    argc = len(argv)
+    suffix = "arguments"
+    endl = ":"
+    if argc == 1:
+        endl = "."
+    elif argv == 2:
+        suffix = "argument"
+
+    if argc == 1:
+        print("0 {}{}".format(suffix, endl))
+    else:
+        print("{:d} {}{}".format(argc - 1, suffix, endl))
+        for i in range(1, argc):
+            print("{:d}: {}".format(i, argv[i]))
