9ea1625865203453a5008a00fce90c02affc825c
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-07-03 18:52:23 +0100

    Fix to accomodate empty lists

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     last = len(my_list) - 1
-    while (last >= 0):
+    for i in range(last, 0, -1):
         print("{:d}".format(my_list[last]))
-        last -= 1
