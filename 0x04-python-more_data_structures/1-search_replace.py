765e2596af8dcf7ad656646f0d2efe677c7b924b
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-07-06 09:23:42 +0100

    Change algorithm to use list comprehension

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    for elem in new_list:
-        if elem is search:
-            new_list[new_list.index(elem)] = replace
-    return new_list
+    return [replace if elem is search else elem for elem in my_list]
