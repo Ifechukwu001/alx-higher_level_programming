44ebcc0e145c2d924e34e2a207f8159a73f6d071
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-07-19 18:27:53 +0100

    Python script containing class with a setter

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """ Square class with setter to private feild """
+    def __init__(self, size=0):
+        pass
+
+    @property
+    def size_s(self):
+        return self.__size
+
+    @size_s.setter
+    def size_s(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
