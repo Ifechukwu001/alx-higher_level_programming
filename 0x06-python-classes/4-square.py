892477b7d08055301b4da799713f34bb1d9eb784
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-07-20 13:13:03 +0100

    Fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
+"""A module containing a Square class.
+
+"""
 class Square:
-    """ Square class with setter and getter to private feild """
+    """Square class with setter and getter to private feild.
+
+    """
     def __init__(self, size=0):
+        """init method of the class.
+
+        Args:
+            size (int): Width of the square.
+
+        """
         self.size = size
 
     @property
     def size(self):
+        """int: a getter for the size attribute.
+
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if size is less than zero.
+
+        """
         return self.__size
 
     @size.setter
@@ -17,4 +35,10 @@ class Square:
         self.__size = value
 
     def area(self):
+        """area method of the class.
+
+        Returns:
+            int: Area of the square.
+
+        """
         return self.__size * self.__size
