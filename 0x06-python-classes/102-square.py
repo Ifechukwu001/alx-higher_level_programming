3e617b9618d5a80f3cecb0b8ea2858d8d316224a
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-07-20 15:34:02 +0100

    Fix

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
+"""A module containing a Square class
+
+"""
 class Square:
-    """ Square class with setter and getter to private feild """
+    """A Square class
+
+    """
     def __init__(self, size=0):
+        """init method of the class.
+
+        Args:
+            size (int): Width of the square.
+
+        """
         self.size = size
 
     @property
     def size(self):
+        """int: Size of the square.
+
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if size is less than zero.
+
+        """
         return self.__size
 
     @size.setter
@@ -17,7 +35,64 @@ class Square:
         self.__size = value
 
     def area(self):
+        """area method of the class.
+
+        Returns:
+            int: area of the square.
+
+        """
         return self.__size * self.__size
 
-    def __repr__(self):
-        return area()
+    def __eq__(self, other):
+        """implements '=='
+
+        Returns:
+            bool: True or False
+
+        """
+        return self.__size == other.size
+
+    def __ne__(self, other):
+        """implements '!='
+
+        Returns:
+            bool: True or False
+
+        """
+        return self.__size != other.size
+
+    def __ge__(self, other):
+        """implements '>='
+
+        Returns:
+            bool: True or False
+
+        """
+        return self.__size >= other.size
+
+    def __le__(self, other):
+        """implements '<='
+
+        Returns:
+            bool: True or False
+
+        """
+        return self.__size <= other.size
+
+    def __gt__(self, other):
+        """implements '>'
+
+        Returns:
+            bool: True or False
+
+        """
+        return self.__size > other.size
+
+    def __lt__(self, other):
+        """implements '<'
+
+        Returns:
+            bool: True or False
+
+        """
+        return self.__size < other.size
