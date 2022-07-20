f81ec1628d5dda3f680e97e21769b3eda287b27a
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-07-20 12:55:25 +0100

    Fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""A module containing a Square class.
+
+"""
 class Square:
-    """ Square class with a private feild """
+    """Square class with a private feild.
+
+       Attributes:
+           size (int): Width of the square.
+    """
     def __init__(self, size):
+        """__init__ method of the class.
+
+           Args:
+               size (int): Width of the square.
+        """
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
+"""A module containing a Square class.
+
+"""
 class Square:
-    """ Square class with setter to private feild """
+    """Square class with setter to private feild.
+       
+    Attributes:
+        size (int): Width of the square.
+
+    """
     def __init__(self, size=0):
+        """ init method of the class.
+
+        Args:
+            size (int): Width of the square.
+
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if size is less than zero.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
