694dd35df3e848cfd9a16e835b61631ac5a647aa
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-07-20 16:27:17 +0100

    Python script containing a class recreated from bytecodes

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""A module containing a MagicClass class
+
+"""
+import math
+
+class MagicClass:
+    """A MagicClass class
+
+    """
+    def __init__(self, radius):
+        """init method.
+
+        Args:
+            radius (int or float): Radius of the circle.
+
+        Raises:
+            TypeError: if radius is not a number.
+
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """area method of class.
+
+        Returns:
+            int: area of a circle.
+
+        """
+        return self.__radius ** 2 * math.pi
+        
+    def circumference(self):
+        """circumference method of class.
+
+        Returns:
+            int: Circumference of a circle.
+
+        """
+        return (2 * math.pi) * self.__radius
