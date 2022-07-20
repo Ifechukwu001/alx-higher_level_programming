3544d6f23f448072dfdf0b1b4aa421f645446d42
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-07-20 13:03:58 +0100

    Fix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
+"""A module containing a Square class
+
+"""
 class Square:
-    """ Square class with setter to private feild """
+    """Square class with setter to private feild
+
+    """
     def __init__(self, size=0):
+        """init method.
+
+        Args:
+            size (int): Width of the square.
+
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if size is less than zero.
+
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
@@ -9,4 +24,10 @@ class Square:
         self.__size = size
 
     def area(self):
+        """A method that calculates the area of the square.
+
+        Return:
+            int: Area of the square.
+
+        """
         return self.__size * self.__size
