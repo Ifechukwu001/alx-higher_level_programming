06b3bd4264c17d2d1bdb569bf6320738cd0a588f
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-07-26 16:47:31 +0100

    Python module containing function that prints a square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A module containing a function that prints a square.
+
+>>> print_square(2)
+##
+##
+
+"""
+
+
+def print_square(size):
+    """A function that prints a square.
+
+    Args:
+        size (int): Length of the square.
+
+    Raises:
+        TypeError: if size is not an integer or is a float less than zero.
+        ValueError: if size is less than zero.
+
+    """
+    if type(size) is not int:
+        if type(size) is float and size >= 0:
+            size = int(size)
+        else:
+            raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        if i != size - 1:
+            print()
