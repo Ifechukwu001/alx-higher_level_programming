d4a326122e7af552e0fbb0697b7f0fe6a305c0d3
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-07-27 16:23:53 +0100

    Fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -27,6 +27,8 @@ def matrix_divided(matrix, div):
     """
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
+    if (div == div + 1) or (div != div):
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
@@ -45,6 +47,9 @@ of integers/floats")
         new_row = []
         for column in row:
             if type(column) is not int and type(column) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) \
+of integers/floats")
+            if (column == column + 1) or (column != column):
                 raise TypeError("matrix must be a matrix (list of lists) \
 of integers/floats")
             new_column = round(column / div, 2)
