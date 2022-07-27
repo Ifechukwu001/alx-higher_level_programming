df175ea9ab58b00319e92a42d74e6842463ce92c
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-07-27 16:01:18 +0100

    Fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -20,9 +20,11 @@ def add_integer(a, b=98):
         int: Sum of a and b.
 
     """
-    if type(a) is not int and type(a) is not float:
+    if (type(a) is not int and type(a) is not float) or \
+(a == a + 1) or (a != a):
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if (type(b) is not int and type(b) is not float) or \
+(b == b + 1) or (b != b):
         raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
