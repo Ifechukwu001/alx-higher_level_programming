a740af139bf3853afa885f2d1e4d252a9b0468d8
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-08-01 23:26:14 +0100

    Python module containing a class with an unimplemented method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A module containing a BaseGeometry class
+
+"""
+
+
+class BaseGeometry:
+    """An empty class
+
+    """
+    def area(self):
+        """A non-functional method
+
+        Raises:
+            Exception: As it was not implemented.
+
+        """
+        raise Exception("area() is not implemented")
