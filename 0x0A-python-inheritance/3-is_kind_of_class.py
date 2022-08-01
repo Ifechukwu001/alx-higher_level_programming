7497b2171df007760482e9cb4757f60b5bb54a90
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2022-08-01 23:02:36 +0100

    Python module containing function that checks if an object is an instance of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A module containing function that checks if an object \
+is an instance of a class
+
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if an obj is an instance of a_class.
+
+    """
+    return isinstance(obj, a_class)
