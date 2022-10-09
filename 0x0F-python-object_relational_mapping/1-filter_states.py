272c0c7726e028c9dcaed1ece846c184372afd81
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2022-10-09 23:41:36 +0100

    Gets states that starts with "N"

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module that connects to a MySQL server and retrieves information
+from the database.
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    user_name = argv[1]
+    pass_word = argv[2]
+    data_base = argv[3]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=user_name,
+                         passwd=pass_word,
+                         db=data_base)
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%'\
+                    ORDER BY states.id")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
