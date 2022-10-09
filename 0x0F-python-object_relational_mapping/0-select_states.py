6b09f60276451bd518092826a641bd3e786b5f2b
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2022-10-09 23:26:06 +0100

    Gets rows in a table ordered by id

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,18 +5,19 @@ from the database.
 import MySQLdb
 from sys import argv
 
-user_name = argv[1]
-pass_word = argv[2]
-data_base = argv[3]
+if __name__ == "__main__":
+    user_name = argv[1]
+    pass_word = argv[2]
+    data_base = argv[3]
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user=user_name,
-                     passwd=pass_word,
-                     db=data_base)
-cursor = db.cursor()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=user_name,
+                         passwd=pass_word,
+                         db=data_base)
+    cursor = db.cursor()
 
-cursor.execute("SELECT * FROM states ORDER BY states.id")
-states = cursor.fetchall()
-for state in states:
-    print(state)
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
