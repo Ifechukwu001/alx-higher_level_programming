3dc9ce978e975d6187a9ad705bf2dac801039388
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2022-11-15 07:32:15 +0100

    Read and print

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* Ouputs content of a file. */
+
+const fs = require('fs');
+const { argv } = require('node:process');
+
+const filename = argv[2];
+
+fs.readFile(filename, 'utf8', function (err, data) {
+  if (err) { console.log(err); } else { console.log(data); }
+});
