8be64ee197623a18e321196d4035e8ff4fc36cac
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2022-11-25 21:06:29 +0100

    Fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@ const { argv } = require('node:process');
 
 const argvLen = argv.length - 2;
 
-if (argvLen === 0) {
+if (argvLen == 0) {
   console.log('No argument');
-} else if (argvLen === 1) {
+} else if (argvLen == 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
