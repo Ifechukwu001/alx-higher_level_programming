f922e33641c8529871398fc5c28ae7d31b3f10ab
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-01-20 11:12:51 +0100

    Revert change

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 /* Ouputs content of a file. */
 
-const fs = require('node:fs');
+const { readFile } = require('node:fs');
 const { argv } = require('node:process');
 
 const filename = argv[2];
 
-fs.readFile(filename, 'utf8', function (err, data) {
+readFile(filename, 'utf8', function (err, data) {
   if (err) { console.log(err); } else { console.log(data); }
 });
